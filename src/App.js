f22a0b13bf21a964e1d6f87e4ac7bd8be910748b
Tee Wen Seng <wenseng@Tee.localdomain>
2022-08-22 16:34:01 +0800

    test again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Change <code>src/App.js</code> to apply changes.
+         Edit <code>src/App.js</code> to apply changes.
         </p>
         <a
           className="App-link"
